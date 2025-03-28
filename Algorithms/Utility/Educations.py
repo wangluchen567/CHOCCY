fd6731dd462b295d476b62458d5b462897c4ea0d
wangluchen567 <wangluchen567@qq.com>
2025-03-28 09:29:07 +0800

    优化代码

--- Algorithms/Utility/Educations.py
+++ Algorithms/Utility/Educations.py
@@ -2,7 +2,7 @@
 教育操作(用于混合算法)
 Educations Operator
 Copyright (c) 2024 LuChen Wang
-[Software Name] is licensed under Mulan PSL v2.
+CHOCCY is licensed under Mulan PSL v2.
 You can use this software according to the terms and conditions of the Mulan
 PSL v2.
 You may obtain a copy of Mulan PSL v2 at:
--- Algorithms/Utility/Mutations.py
+++ Algorithms/Utility/Mutations.py
@@ -2,7 +2,7 @@
 变异算子
 Mutation Operator
 Copyright (c) 2024 LuChen Wang
-[Software Name] is licensed under Mulan PSL v2.
+CHOCCY is licensed under Mulan PSL v2.
 You can use this software according to the terms and conditions of the Mulan
 PSL v2.
 You may obtain a copy of Mulan PSL v2 at:
