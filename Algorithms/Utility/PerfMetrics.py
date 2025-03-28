6746c6f6bb59e82b0facc361fbef134082c80c25
wangluchen567 <wangluchen567@qq.com>
2025-03-28 14:49:02 +0800

    使用numba加速超体积指标的计算

--- Algorithms/Utility/PerfMetrics.py
+++ Algorithms/Utility/PerfMetrics.py
@@ -140,45 +140,61 @@ def cal_HV(objs, optimum=None):
     # 根据参考点向量规范化目标值
     f_min = np.min(np.vstack((np.min(objs, axis=0), np.zeros([1, M]))), axis=0)
     f_max = np.max(refer_array, axis=0)
-    objs_normalized = (objs - f_min) / np.tile((f_max - f_min) * 1.1, (N, 1))
-    objs_normalized = objs_normalized[np.all(objs_normalized <= 1, axis=1)]
+    objs_norm = (objs - f_min) / np.tile((f_max - f_min) * 1.1, (N, 1))
+    objs_norm = objs_norm[np.all(objs_norm <= 1, axis=1)]
     # 参考点向量设置为全1向量
     ref_point = np.ones(M)
     # 如果目标值矩阵为空，超体积为0
-    if objs_normalized.size == 0:
+    if objs_norm.size == 0:
         score = 0
     elif M < 4:
         # 计算精确的HV值
-        pl = np.unique(objs_normalized, axis=0)
-        s = [[1, pl]]
-        for k in range(M - 1):
-            s_ = []
-            for i in range(len(s)):
-                stemp = Slice(s[i][1], k, ref_point)
-                for j in range(len(stemp)):
-                    temp = [[stemp[j][0] * s[i][0], np.array(stemp[j][1])]]
-                    s_ = Add(temp, s_)
-            s = s_
-        score = 0
-        for i in range(len(s)):
-            p = Head(s[i][1])
-            score = score + s[i][0] * np.abs(p[-1] - ref_point[-1])
+        score = cal_HV_accurate(objs_norm, ref_point)
     else:
         # 通过蒙特卡罗方法估计HV值
-        sample_num = 1e6
-        max_value = ref_point
-        min_value = np.min(objs_normalized, axis=0)
-        samples = np.random.uniform(np.tile(min_value, (sample_num, 1)), np.tile(max_value, (sample_num, 1)))
-        for i in range(len(objs_normalized)):
-            domi = np.ones(len(samples), dtype=bool)
-            m = 0
-            while m <= M - 1 and np.any(domi):
-                b = objs_normalized[i][m] <= samples[:, m]
-                domi = domi & b
-                m += 1
-            samples = samples[~domi]
-        score = np.prod(max_value - min_value) * (1 - len(samples) / sample_num)
+        score = cal_HV_estimated(objs_norm, ref_point)
+    return score
 
+
+def cal_HV_estimated_(objs_norm, ref_point, sample_num=1e6):
+    """使用蒙特卡罗方法估计HV值"""
+    sample_num = int(sample_num)
+    max_value = ref_point
+    min_value = np.min(objs_norm, axis=0)
+    samples = np.random.uniform(np.tile(min_value, (sample_num, 1)), np.tile(max_value, (sample_num, 1)))
+    for i in range(len(objs_norm)):
+        domi = np.ones(len(samples), dtype=bool)
+        m = 0
+        while m <= objs_norm.shape[1] - 1 and np.any(domi):
+            b = objs_norm[i][m] <= samples[:, m]
+            domi = domi & b
+            m += 1
+        samples = samples[~domi]
+    score = np.prod(max_value - min_value) * (1 - len(samples) / sample_num)
+    return score
+
+
+def cal_HV_accurate_(objs_norm, ref_point):
+    """
+    计算精确的HV值
+    This code is based on the research presented in
+    "A Faster Algorithm for Calculating Hypervolume"
+    by Lyndon While, Philip Hingston, Luigi Barone, and Simon Huband
+    """
+    pl = np.unique(objs_norm, axis=0)
+    s = [[1, pl]]
+    for k in range(objs_norm.shape[1] - 1):
+        s_ = []
+        for i in range(len(s)):
+            stemp = Slice(s[i][1], k, ref_point)
+            for j in range(len(stemp)):
+                temp = [[stemp[j][0] * s[i][0], np.array(stemp[j][1])]]
+                s_ = Add(temp, s_)
+        s = s_
+    score = 0
+    for i in range(len(s)):
+        p = Head(s[i][1])
+        score = score + s[i][0] * np.abs(p[-1] - ref_point[-1])
     return score
 
 
@@ -276,10 +292,12 @@ def Add(list_: list, s: list) -> list:
 
 try:
     # 尝试导入numba
+    import numba as nb
     from numba import jit
+    from numba.typed import List
 
 
-    @jit(nopython=True)
+    @jit(nopython=True, cache=True)
     def array_equal(arr1, arr2, equal_nan=False):
         """
         检查两个数组是否具有相同的形状和元素。
@@ -305,7 +323,189 @@ try:
 
         return True
 
+
+    @jit(nopython=True, cache=True)
+    def _Head(pl):
+        if pl.shape[0] == 0:
+            return np.empty(0)
+        return pl[0]
+
+
+    @jit(nopython=True, cache=True)
+    def _Tail(pl):
+        if pl.shape[0] <= 1:
+            return np.empty((0, pl.shape[1]))
+        return pl[1:]
+
+
+    @jit(nopython=True, cache=True)
+    def _Add(list_, s):
+        num_s = len(s)
+        found = False
+        new_factor, new_ql = list_[0]
+        for i in range(num_s):
+            old_factor, old_ql = s[i]
+            if array_equal(new_ql, old_ql):
+                s[i] = (old_factor + new_factor, old_ql)
+                found = True
+                break
+        if not found:
+            if num_s == 0:
+                return list_
+            else:
+                s.append((new_factor, new_ql))
+        return s
+
+
+    @jit(nopython=True, cache=True)
+    def _Insert(p, k, pl):
+        m = p.shape[0]
+        ql = np.zeros((0, m), dtype=p.dtype)
+        i = 0
+        while i < pl.shape[0] and pl[i, k] < p[k]:
+            ql = np.vstack((ql, pl[i:i + 1]))
+            i += 1
+        ql = np.vstack((ql, p.reshape(1, -1)))
+        while i < pl.shape[0]:
+            q = pl[i]
+            dominated = True
+            has_less = False
+            for j in range(k, m):
+                if p[j] > q[j]:
+                    dominated = False
+                    break
+                elif p[j] < q[j]:
+                    has_less = True
+            if not (dominated and has_less):
+                ql = np.vstack((ql, q.reshape(1, -1)))
+            i += 1
+        return ql
+
+
+    @jit(nopython=True, cache=True)
+    def _Slice(pl, k, ref_point):
+        s = List()
+        if pl.shape[0] == 0:
+            s.append((1.0, pl))
+            return s
+        p = _Head(pl)
+        pl = _Tail(pl)
+        ql = np.zeros((0, pl.shape[1]), dtype=pl.dtype)
+        while pl.shape[0] > 0:
+            ql = _Insert(p, k + 1, ql)
+            p_next = _Head(pl)
+            list_ = List()
+            delta = np.abs(p[k] - p_next[k])
+            list_.append((delta, ql))
+            s = _Add(list_, s)
+            p = p_next
+            pl = _Tail(pl)
+        ql = _Insert(p, k + 1, ql)
+        list_ = List()
+        delta = np.abs(p[k] - ref_point[k])
+        list_.append((delta, ql))
+        s = _Add(list_, s)
+        return s
+
+
+    def cal_HV_accurate(objs_norm, ref_point):
+        """
+        精确计算HV值(使用numba加速)
+        This code is based on the research presented in
+        "A Faster Algorithm for Calculating Hypervolume"
+        by Lyndon While, Philip Hingston, Luigi Barone, and Simon Huband
+        """
+        # 删除重复项 Remove duplicates
+        objs_norm = np.unique(objs_norm, axis=0)
+        if objs_norm.size == 0:
+            return 0.0
+        return _cal_HV_accurate_numba(objs_norm, ref_point)
+
+
+    @jit(nopython=True, cache=True)
+    def _cal_HV_accurate_numba(objs_norm, ref_point):
+        pl = objs_norm
+        s = List()
+        # Initialize with explicit typing
+        s.append((1.0, pl))
+        for k in range(objs_norm.shape[1] - 1):
+            s_new = List()
+            # 指定元素后才能确定类型
+            s_new.append((1.0, np.empty((0, pl.shape[1]))))
+            for i in range(len(s)):
+                factor, points = s[i]
+                if points.shape[0] == 0:
+                    continue
+                slice_temp = _Slice(points, k, ref_point)
+                for item in slice_temp:
+                    delta, ql = item
+                    if ql.shape[0] == 0:
+                        continue
+                    new_factor = factor * delta
+                    temp = List()
+                    temp.append((new_factor, ql))
+                    s_new = _Add(temp, s_new)
+            s = s_new
+        score = 0.0
+        for i in range(len(s)):
+            factor, points = s[i]
+            if points.shape[0] == 0:
+                continue
+            p = _Head(points)
+            score += factor * np.abs(p[-1] - ref_point[-1])
+        return score
+
+    @jit(nopython=True, parallel=True, cache=True)
+    def cal_HV_estimated(objs_norm, ref_point, sample_num=1e6):
+        """使用蒙特卡罗方法估计HV值（Numba加速版）"""
+        sample_num = int(sample_num)
+        dim = objs_norm.shape[1]
+        n_points = objs_norm.shape[0]
+
+        # 计算最小边界
+        min_values = np.empty(dim)
+        for d in range(dim):
+            min_val = np.inf
+            for i in range(n_points):
+                if objs_norm[i, d] < min_val:
+                    min_val = objs_norm[i, d]
+            min_values[d] = min_val
+
+        # 生成蒙特卡洛样本
+        samples = np.empty((sample_num, dim))
+        for i in nb.prange(sample_num):
+            for d in range(dim):
+                samples[i, d] = np.random.uniform(min_values[d], ref_point[d])
+
+        # 初始化活跃样本标记
+        active = np.ones(sample_num, dtype=nb.bool_)
+
+        # 并行处理每个样本
+        for i in range(n_points):
+            current_obj = objs_norm[i]
+            for j in nb.prange(sample_num):
+                if active[j]:
+                    dominated = True
+                    # 检查是否被当前目标点支配
+                    for m in range(dim):
+                        if samples[j, m] < current_obj[m]:
+                            dominated = False
+                            break
+                    if dominated:
+                        active[j] = False
+
+        # 计算最终结果
+        valid_count = sample_num - np.sum(active)
+        volume = 1.0
+        for d in range(dim):
+            volume *= (ref_point[d] - min_values[d])
+
+        return volume * valid_count / sample_num
+
 except ImportError:
     # 如果导入numba加速库失败，使用原始的函数
     warnings.warn("Optimizing problems without using numba acceleration...")
     array_equal = np.array_equal
+    cal_HV_accurate = cal_HV_accurate_
+    cal_HV_estimated = cal_HV_estimated_
+
