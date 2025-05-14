37e61ed73ea52f290191882bce04c12f17fde520
wangluchen567 <wangluchen567@qq.com>
2025-05-14 15:17:28 +0800

    优化TSPLIB数据集

--- Datasets/Single/TSPLIB/LoadTSPLIB.py
+++ Datasets/Single/TSPLIB/LoadTSPLIB.py
@@ -68,6 +68,71 @@ def load_euc_2d(file_path):
     return data
 
 
-if __name__ == '__main__':
-    data = load_euc_2d('berlin52.tsp')
-    print(data)
+def load_matrix(file_path):
+    """加载给定城市点之间的距离矩阵的数据集"""
+    # 定义一个空字典来存储文件中的信息
+    data = {
+        'name': None,
+        'type': None,
+        'comment': None,
+        'dimension': None,
+        'edge_weight_type': None,
+        'node_coord': [],
+        'dist_matrix': [],
+    }
+    # 初始化一个下三角矩阵信息
+    lower_triangle_data = []
+    # 打开文件并读取内容
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    # 用于存储当前正在解析的节
+    current_section = None
+    # 逐行解析文件内容
+    for line in lines:
+        # 去除行尾的换行符
+        line = line.strip()
+        # 忽略空行
+        if not line:
+            continue
+        if line == 'EOF':
+            # 若到结尾则停止
+            break
+        # 检查是否是节的标题
+        elif (line.startswith('NAME') or
+              line.startswith('TYPE') or
+              line.startswith('COMMENT') or
+              line.startswith('DIMENSION') or
+              line.startswith('EDGE_WEIGHT_TYPE')):
+            key, value = line.split(':')
+            key = key.strip().lower()  # 将键转换为小写
+            if key in ['dimension']:
+                data[key] = int(value)  # 转换为整数
+            else:
+                data[key] = value.strip()  # 去除两端空白字符
+        elif line == 'EDGE_WEIGHT_SECTION':
+            current_section = 'dist_matrix'
+        elif line == 'DISPLAY_DATA_SECTION':
+            current_section = 'node_coord'
+        elif current_section == 'dist_matrix':
+            parts = line.split()
+            parts_data = [float(parts[i]) for i in range(len(parts))]
+            lower_triangle_data.extend(parts_data)
+        elif current_section == 'node_coord':
+            parts = line.split()
+            if len(parts) >= 3:  # 确保行不为空且有足够的数据
+                node_id, x, y = int(parts[0]), float(parts[1]), float(parts[2])
+                data['node_coord'].append([x, y])
+        else:
+            continue
+    # 城市数量
+    n = data['dimension']
+    index = 0  # 数据下标
+    data['dist_matrix'] = np.zeros((n, n))
+    for i in range(n):
+        for j in range(i + 1):  # 遍历下三角（含对角线）
+            data['dist_matrix'][i][j] = lower_triangle_data[index]
+            data['dist_matrix'][j][i] = lower_triangle_data[index]
+            index += 1
+    # 将点坐标位置数据转换为numpy数据
+    data['node_coord'] = np.array(data['node_coord'])
+    return data
--- RunDemos/Single/Solve_TSPLib.py
+++ RunDemos/Single/Solve_TSPLib.py
@@ -1,16 +1,20 @@
 from Problems.Single import TSP
 from Algorithms import View, Comparator
-from Datasets.Single.TSPLIB.LoadTSPLIB import load_euc_2d
 from Algorithms.Single import GA, SA, ACO, HGATSP, FI, GFLS
+from Datasets.Single.TSPLIB.LoadTSPLIB import load_euc_2d, load_matrix
 
 if __name__ == '__main__':
-    # 读取TSPLIB数据集
-    tsp_data = load_euc_2d("../../Datasets/Single/TSPLIB/berlin52.tsp")
+    # 读取TSPLIB数据集（给定城市坐标位置）
+    tsp_data = load_euc_2d("../../Datasets/Single/TSPLIB/eil51.tsp")
     problem = TSP(num_dec=tsp_data['dimension'], data=tsp_data['node_coord'], round_to_int=True)
+    # 读取TSPLIB数据集（给定城市坐标的距离矩阵）
+    # tsp_data = load_matrix("../../Datasets/Single/TSPLIB/gr120.tsp")
+    # problem = TSP(num_dec=tsp_data['dimension'], data=tsp_data['dist_matrix'], is_dist_mat=True, round_to_int=True)
+    # 初始化算法字典
     algorithms = dict()
     pop_size, max_iter = 100, 1000
     algorithms['GA'] = GA(pop_size, max_iter)
-    algorithms['SA'] = SA(pop_size, max_iter, init_temp=1e6)
+    algorithms['SA'] = SA(pop_size, max_iter)
     algorithms['ACO'] = ACO(pop_size, max_iter)
     algorithms['HGA-TSP'] = HGATSP(pop_size, max_iter)
     algorithms['FI'] = FI()
