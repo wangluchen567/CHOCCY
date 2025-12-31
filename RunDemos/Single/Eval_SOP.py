b758ca8c7b6bf7966440bcd61f5598876548d584
wangluchen567 <wangluchen567@qq.com>
2025-12-31 18:07:44 +0800

    优化代码

--- RunDemos/Single/Eval_SOP.py
+++ RunDemos/Single/Eval_SOP.py
@@ -9,15 +9,15 @@ if __name__ == '__main__':
     # 初始化算法字典集合
     algorithms = dict()
     # 定义算法的参数(参数统一)
-    pop_size, max_iter = 100, 100
+    pop_size, max_iter = 50, 1000
     # 在算法字典集合中加入算法
     algorithms['GA'] = GA(pop_size, max_iter)
     algorithms['SA'] = SA(pop_size, max_iter)
     algorithms['PSO'] = PSO(pop_size, max_iter)
-    algorithms['DE/rand/1'] = DE(pop_size, max_iter, operator_type=DE.RAND1)
-    algorithms['DE/rand/2'] = DE(pop_size, max_iter, operator_type=DE.RAND2)
-    algorithms['DE/best/1'] = DE(pop_size, max_iter, operator_type=DE.BEST1)
-    algorithms['DE/best/2'] = DE(pop_size, max_iter, operator_type=DE.BEST2)
+    algorithms['DE/rand/1'] = DE(pop_size, max_iter, operator_type=DE.RAND_1)
+    algorithms['DE/rand/2'] = DE(pop_size, max_iter, operator_type=DE.RAND_2)
+    algorithms['DE/best/1'] = DE(pop_size, max_iter, operator_type=DE.BEST_1)
+    algorithms['DE/best/2'] = DE(pop_size, max_iter, operator_type=DE.BEST_2)
     # 使用评估器评估多种算法在多个问题上的表现
     evaluator = Evaluator(problems, algorithms, num_run=30, same_init=True)
     evaluator.run()
